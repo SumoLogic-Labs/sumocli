82362a5fd4a841455cf6916618767aae4dcef292
Kyle Jackson <kyle@thepublicclouds.com>
2020-11-09 08:58:55 +1000

    Adding config file support

--- pkg/cmd/login/login.go
+++ pkg/cmd/login/login.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"github.com/manifoldco/promptui"
 	"github.com/spf13/cobra"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
 func NewCmdLogin() *cobra.Command {
@@ -13,9 +16,12 @@ func NewCmdLogin() *cobra.Command {
 		RunE: func(cmd *cobra.Command, args []string) error {
 			fmt.Println("Sumocli requires an access key and secret key.")
 			fmt.Println("Sumocli will store the access key and secret key in plain text in" +
-				"in the following file for use by subsequent commands:")
-			userConfirmation()
+				" the following file for use by subsequent commands:")
 			// TODO: Add the file path here
+			configFile := configPath()
+			fmt.Println(configFile)
+			confirmation := userConfirmation()
+			fmt.Println(confirmation)
 			// TODO: Access Key and Secret
 			// TODO: write to disk with Viper
 			return nil
@@ -25,27 +31,32 @@ func NewCmdLogin() *cobra.Command {
 	return cmd
 }
 
-func userConfirmation() {
-	validate := func(input string) error {
-		if input != "yes" {
-			fmt.Println("Error: Login Cancelled")
-		}
-		return nil
-	}
+func configPath() string {
+	var filePath string = ".sumocli/credentials/creds.json"
+	homeDirectory, _ := os.UserHomeDir()
+	configFile := filepath.Join(homeDirectory, filePath)
+	fmt.Println(configFile)
+	return configFile
+}
 
+func userConfirmation() bool {
 	prompt := promptui.Prompt{
-		Label:    "Do you want to proceed?",
-		Validate: validate,
+		Label: "Do you want to proceed?",
 	}
 
 	result, err := prompt.Run()
+	resultLower := strings.ToLower(result)
 
 	if err != nil {
 		fmt.Printf("Prompt failed %v\n", err)
 	}
 
-	fmt.Print(result)
-
+	if resultLower == "yes" {
+		return true
+	} else {
+		fmt.Println("Error: Login cancelled")
+		return false
+	}
 }
 
 func ReadLoginFile() {
@@ -53,5 +64,5 @@ func ReadLoginFile() {
 }
 
 func writeLoginFile() {
-
+	// https://github.com/spf13/viper
 }
