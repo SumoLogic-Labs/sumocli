ae698133f388d89850dedd5ab90bff41adc15699
Kyle Jackson <kyle@thepublicclouds.com>
2020-12-05 13:27:38 +1000

    Adding Azure SDK functionality

--- pkg/cmd/azure/create/create.go
+++ pkg/cmd/azure/create/create.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"github.com/Azure/azure-sdk-for-go/profiles/latest/resources/mgmt/features"
+	"github.com/Azure/azure-sdk-for-go/profiles/latest/storage/mgmt/storage"
 	"github.com/Azure/go-autorest/autorest/to"
 	"github.com/spf13/cobra"
 	"github.com/wizedkyle/sumocli/pkg/cmd/factory"
@@ -11,18 +12,41 @@ import (
 )
 
 func NewCmdAzureCreate() *cobra.Command {
+	var (
+		prefix     string
+		diagnostic bool
+		metrics    bool
+		blob       bool
+	)
+
 	cmd := &cobra.Command{
 		Use:   "create",
 		Short: "Create Azure infrastructure to collect logs or metrics",
 		Run: func(cmd *cobra.Command, args []string) {
 			logger := logging.GetLoggerForCommand(cmd)
 			logger.Debug().Msg("Create Azure infrastructure request started")
-			ctx := context.Background()
-			createResourceGroup(ctx, "sumocli-log-collection")
+			if blob == true {
+				azureCreateBlobCollection(prefix)
+			} else if metrics == true {
+
+			} else {
+				fmt.Println("Please select either --logs or --metrics")
+			}
 			logger.Debug().Msg("Create Azure infrastructure request finished.")
 		},
 	}
+
+	cmd.Flags().StringVar(&prefix, "prefix", "", "Name of the resource")
 	return cmd
+}
+
+func azureCreateBlobCollection(prefix string) {
+	ctx := context.Background()
+	logsName := "scliblob"
+	rgName := logsName + prefix
+	sgName := logsName + prefix + "logs"
+	createResourceGroup(ctx, rgName)
+	sgAccount, err := createStorageAccount(ctx, rgName, sgName)
 }
 
 /*
@@ -46,9 +70,35 @@ func createResourceGroup(ctx context.Context, rgName string) {
 		})
 
 	if err != nil {
-		fmt.Println("cannot create resource group " + rgName)
-		fmt.Println(err)
+		fmt.Errorf("cannot create resource group %v: %v", rgName, err)
 	}
-	ctx.Done()
 	fmt.Println("Created or updated resource group " + rgName)
+}
+
+func createStorageAccount(ctx context.Context, rgName string, sgName string) (storage.Account, error) {
+	fmt.Println("Creating or updating storage account " + sgName)
+	var sku *storage.Sku
+	sku.Name = "Standard_LRS"
+	sku.Tier = "Standard"
+	sgClient := factory.GetStorageClient()
+	sgAccount, err := sgClient.Create(
+		ctx,
+		rgName,
+		sgName,
+		storage.AccountCreateParameters{
+			Sku:      sku,
+			Location: to.StringPtr(factory.Location),
+			Tags:     factory.AzureLogTags(),
+		})
+
+	if err != nil {
+		fmt.Errorf("cannot create resource group %v: %v", sgName, err)
+	}
+
+	sgAccount.WaitForCompletionRef(ctx, sgClient.Client)
+	if err != nil {
+		fmt.Errorf("cannot create resource group %v: %v", sgName, err)
+	}
+
+	return sgAccount.Result(sgClient)
 }
--- pkg/cmd/factory/azureclients.go
+++ pkg/cmd/factory/azureclients.go
@@ -2,6 +2,7 @@ package factory
 
 import (
 	"github.com/Azure/azure-sdk-for-go/profiles/latest/resources/mgmt/features"
+	"github.com/Azure/azure-sdk-for-go/profiles/latest/storage/mgmt/storage"
 )
 
 func GetResourceGroupClient() features.ResourceGroupsClient {
@@ -10,4 +11,20 @@ func GetResourceGroupClient() features.ResourceGroupsClient {
 	rgClient.Authorizer = auth
 	rgClient.AddToUserAgent(userAgent())
 	return rgClient
+}
+
+func GetStorageClient() storage.AccountsClient {
+	sgClient := storage.NewAccountsClient(SubscriptionId)
+	auth, _ := AzureRMAuth()
+	sgClient.Authorizer = auth
+	sgClient.AddToUserAgent(userAgent())
+	return sgClient
+}
+
+func GetStorageTableClient() storage.TableClient {
+	sgTableClient := storage.NewTableClient(SubscriptionId)
+	auth, _ := AzureRMAuth()
+	sgTableClient.Authorizer = auth
+	sgTableClient.AddToUserAgent(userAgent())
+	return sgTableClient
 }
