818542331e9b37239c9c01b876a0a8eac50b1398
Kyle Jackson <kyle@thepublicclouds.com>
2021-09-25 14:52:57 +1000

    reworking build script to support debian packages

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -23,15 +23,41 @@ Write-Host $build
 
 # Add goarchitecture if statement
 
+if ($linux -eq $true) {
+    Write-Host "Compiling Linux $goarchitecture binary"
+    $env:GOOS="linux"; $env:GOARCH=$goarchitecture; go build -ldflags `
+    "-X 'github.com/SumoLogic-Incubator/sumocli/internal/build.Version=$version'
+      -X 'github.com/SumoLogic-Incubator/sumocli/internal/build.Build=$build'" `
+    ./cmd/sumocli
+    if ($version -ne "DEV" -and $build -ne "DEV") {
+        Write-Host "=> Creating deb package"
+        mkdir -p ~/deb/sumocli_$version-1_amd64/usr/bin
+        cp sumocli ~/deb/sumocli_$version-1_amd64/usr/bin
+        Write-Host "=> Creating DEBIAN control file"
+        mkdir -p ~/deb/sumocli_$version-1_amd64/DEBIAN
+        $controlFile = @"
+Package: sumocli
+Version: $version
+Maintainer: $maintainer
+Architecture: amd64
+Homepage: https://github.com/SumoLogic-Incubator/sumocli
+Description: Sumocli is a CLI application written in Go that allows you to manage your Sumo Logic tenancy from the command line.
+"@
+        Set-Content -Path ~/deb/sumocli_$version-1_amd64/DEBIAN/control -Value $controlFile
+        Write-Host "=> Building deb package"
+        dpkg --build ~/deb/sumocli_$version-1_$goarchitecture
+    }
+}
+
 if ($windows -eq $true) {
-    Write-Host "=> Compiling Windows " + $goarchitecture + " binary"
+    Write-Host "=> Compiling Windows $goarchitecture binary"
     $env:GOOS="windows"; $env:GOARCH=$goarchitecture; go build -ldflags `
     "-X 'github.com/SumoLogic-Incubator/sumocli/internal/build.Version=$version'
       -X 'github.com/SumoLogic-Incubator/sumocli/internal/build.Build=$build'" `
     ./cmd/sumocli
     if ($version -ne "DEV" -and $build -ne "DEV") {
         Write-Host "=> preparing to sign code"
-        Write-Host "=> Install azuresigntool"
+        Write-Host "=> Installing azuresigntool"
         dotnet tool install --global AzureSignTool --version 3.0.0
         Write-Host "=> Signing Windows binary with Azure Key Vault"
         azuresigntool sign --description-url "https://github.com/SumoLogic-Incubator/sumocli" --file-digest sha256 `
