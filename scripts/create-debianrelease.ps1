a56b1ad14cab9ca352adb0022047aa1239edfdcb
Kyle Jackson <kyle@thepublicclouds.com>
2021-09-26 21:44:54 +1000

    reworking build script to support debian packages

--- scripts/create-debianrelease.ps1
+++ scripts/create-debianrelease.ps1
@@ -6,6 +6,7 @@ param (
 Get-ChildItem -Path ~/aptsumocli/dists/stable/main -recurse -File | Foreach-Object {
     $hash = Get-FileHash $_.FullName -Algorithm $algorithm
     $relativePath = Resolve-Path -Path ~/aptsumocli/dists/stable | Select-Object -ExpandProperty Path
+    Write-Host $relativePath
     $fileName = $_.FullName.TrimStart($relativePath + "/")
     Write-Host $hash.Hash $_.Length $fileName
 }
