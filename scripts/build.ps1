ffcfabb33540b6424acc5e4decf33bb7c92a5074
Kyle Jackson <kyle@thepublicclouds.com>
2021-09-26 21:04:58 +1000

    reworking build script to support debian packages

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -83,13 +83,10 @@ SHA256:
 $(pwsh "$PSScriptRoot/create-debianrelease.ps1" -algorithm SHA256 | Out-String)
 "@
             echo $releaseFile > ~/aptsumocli/dists/stable/Release
-            cat ~/aptsumocli/dists/stable/Release
             Write-Host "=> Signing release file"
             cat ~/aptsumocli/dists/stable/Release | gpg --default-key "Kyle Jackson" -abs > ~/aptsumocli/dists/stable/Release.gpg
-            cat ~/aptsumocli/dists/stable/Release.gpg
             Write-Host "=> Creating InRelease file"
             cat ~/aptsumocli/dists/stable/Release | gpg --default-key "Kyle Jackson" -abs --clearsign > ~/aptsumocli/dists/stable/InRelease
-            cat ~/aptsumocli/dists/stable/InRelease
             Write-Host "Syncing local aptsumocli repo to S3"
             aws s3 sync ~/aptsumocli/ s3://aptsumocli
             # Sync contents of repo back to the S3 bucket
