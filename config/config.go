dc01bbc4fd7106ec020f85f7e9a147ae09abd7e2
Kyle Jackson <kyle@thepublicclouds.com>
2022-01-10 20:07:47 +1000

    Removed redundant code, added date to version

--- config/config.go
+++ config/config.go
@@ -2,9 +2,14 @@ package config
 
 import (
 	"github.com/SumoLogic-Labs/sumocli/internal/authentication"
+	"github.com/SumoLogic-Labs/sumocli/internal/build"
 	"github.com/SumoLogic-Labs/sumologic-go-sdk/service/cip"
 	"net/http"
 	"time"
+)
+
+var (
+	userAgent = "Sumocli " + build.Version
 )
 
 func GetSumoLogicSDKConfig() *cip.APIClient {
@@ -23,4 +28,8 @@ func GetSumoLogicSDKConfig() *cip.APIClient {
 		},
 	}
 	return &client
+}
+
+func GetUserAgent() string {
+	return userAgent
 }
